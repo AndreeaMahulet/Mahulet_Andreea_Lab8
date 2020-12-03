2e9c23de68c5b0eafa9997b7da4dc7b4193f9be2
ANDREEA MĂHULEȚ <andreea.mahulet@stud.ubbcluj.ro>
2020-12-03 17:06:47 +0200

    lab9

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Mahulet_Andreea_Lab8.Models;
 
 namespace Mahulet_Andreea_Lab8.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Mahulet_Andreea_Lab8.Data.Mahulet_Andreea_Lab8Context _context;
 
@@ -21,16 +21,48 @@ namespace Mahulet_Andreea_Lab8.Pages.Books
 
         public IActionResult OnGet()
         {
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
             return Page();
         }
 
         [BindProperty]
         public Book Book { get; set; }
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
+        {
+            var newBook = new Book();
+            if (selectedCategories != null)
+            {
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
+            }
+            if (await TryUpdateModelAsync<Book>(
+            newBook,
+            "Book",
+            i => i.Title, i => i.Author,
+            i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                _context.Book.Add(newBook);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            PopulateAssignedCategoryData(_context, newBook);
+            return Page();
+        }
+
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        /*public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
@@ -41,6 +73,7 @@ namespace Mahulet_Andreea_Lab8.Pages.Books
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
-        }
+        }*/
+    
     }
 }
